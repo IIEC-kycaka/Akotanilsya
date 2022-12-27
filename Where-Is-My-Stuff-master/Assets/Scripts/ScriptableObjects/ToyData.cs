54dd7640583776349e8f6f0e659aee30530bb0a8
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2022-12-27 16:30:56 +0300

    del

--- Where-Is-My-Stuff-master/Assets/Scripts/ScriptableObjects/ToyData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Toy", menuName = "Game/Toy")]
-public class ToyData : ScriptableObject {
-	public Sprite sprite;
-	public int points = 10;
-	public float randomWeight = 1f;
-}
--- Where-Is-My-Stuff-master/Assets/Scripts/Utils/Util.cs
+++
@@ -1,7 +0,0 @@
-public static class Util {
-	public static void Swap<T>(ref T a, ref T b) {
-		T temp = a;
-		a = b;
-		b = temp;
-	}
-}
