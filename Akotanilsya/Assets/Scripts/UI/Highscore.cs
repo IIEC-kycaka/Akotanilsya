92b5966bf1f57437cb2082a11e3e767b3eba64e3
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2022-12-27 16:33:53 +0300

    zaliv

---
+++ Akotanilsya/Assets/Scripts/UI/Highscore.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Highscore : MonoBehaviour {
+	public GameObject scoreOneContainer;
+	public TextMeshProUGUI scoreOne;
+	public GameObject scoreTwoContainer;
+	public TextMeshProUGUI scoreTwo;
+	public GameObject scoreThreeContainer;
+	public TextMeshProUGUI scoreThree;
+
+	public SceneReference menuScene;
+	public SceneReference gameScene;
+
+	private string[] scores;
+
+	private void Start() {
+		scores = PlayerPrefs.GetString("highscore", "0-0-0").Split('-');
+
+		UpdateTextValues();
+	}
+
+	private void UpdateTextValues() {
+		scoreOneContainer.SetActive(Int16.Parse(scores[0]) > 0);
+		scoreOne.text = Int16.Parse(scores[0]) > 0 ? scores[0] : "";
+
+		scoreTwoContainer.SetActive(Int16.Parse(scores[1]) > 0);
+		scoreTwo.text = Int16.Parse(scores[1]) > 0 ?scores[1] : "";
+
+		scoreThreeContainer.SetActive(Int16.Parse(scores[2]) > 0);
+		scoreThree.text = Int16.Parse(scores[2]) > 0 ? scores[2] : "";
+	}
+
+	public void ReturnToMenu() {
+		SceneManager.LoadScene(menuScene);
+	}
+
+	public void Retry() {
+		SceneManager.LoadScene(gameScene);
+	}
+}
